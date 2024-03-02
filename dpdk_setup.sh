bc22626e9eb1bc5a6324248bda3fdb627e130ed6
adithyam <adithyam@node-1.adithyam-194428.heartbeat-pg0.utah.cloudlab.us>
2024-03-02 10:37:36 -0700

    Removing DPDK tar file after extracting

--- dpdk_setup.sh
+++ dpdk_setup.sh
@@ -3,6 +3,7 @@
 # Download the DPDK source file
 wget http://fast.dpdk.org/rel/dpdk-23.07.tar.xz
 tar xJf dpdk-23.07.tar.xz
+rm dpdk-23.07.tar.xz
 cd dpdk-23.07
 # Configure DPDK build 
 meson setup build
